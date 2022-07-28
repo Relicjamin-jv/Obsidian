eecb52c2b4f4434ce659cb0e6e8031c9f827c673
Relicjamin-jv <collincampbell97@proton.me>
2022-07-28 15:09:19 -0400

    udpates

--- githubUpdate.sh
+++ githubUpdate.sh
@@ -3,5 +3,5 @@
 git add *
 git commit --all -m udpates
 git push
-echo "Relicjamin-jv"
-echo "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"
+echo "Relicjamin-jv" | git
+echo "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" | git
